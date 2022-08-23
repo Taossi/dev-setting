2cf4e8d257e86225f560cbf47d3910f909fbe9e4
hantao.fang <hantao.fang@shopee.com>
2022-08-23 16:36:58 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # https://segmentfault.com/a/1190000014992947 
 
-path="/ZshSource"
+dir=$(pwd)
+path="$dir/ZshSource"
 mkdir -p $path
+echo $dir
 
 ###########################
 # oh my zsh install, using in iterm2
@@ -10,20 +12,20 @@ mkdir -p $path
 ###########################
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" 
 
-# highlight
-git clone https://gitee.com/mo2/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 # install plugin
 pip install powerline-status --user
 git clone https://github.com/powerline/fonts.git --depth=1
 cd $path/fonts
 ./install.sh 
 
-cd ..
+cd $path
 git clone https://github.com/fcamblor/oh-my-zsh-agnoster-fcamblor.git
 cd $path/oh-my-zsh-agnoster-fcamblor
 ./install
 
+cd ~/.oh-my-zsh/custom/plugins/
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
+# highlight
 cd ~/.oh-my-zsh/custom/plugins/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
